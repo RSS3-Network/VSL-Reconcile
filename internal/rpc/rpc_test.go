a8f7192e468406b667d6a07ea57e8999848687fb
Nya Candy <dev@candinya.com>
2024-05-14 15:48:08 +0800

    feat: add tests

---
+++ internal/rpc/rpc_test.go
@@ -0,0 +1,193 @@
+package rpc
+
+import (
+	"github.com/rss3-network/vsl-reconcile/test"
+	"testing"
+)
+
+func TestCheckSequencerActive(t *testing.T) {
+	t.Parallel()
+
+	// Prepare mock sequencer
+	ms, endpoint, err := test.NewMockSequencer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ms.Close()
+
+	// Situation 1: sequencer disable admin endpoints
+	ms.SetIsWithAdmin(false)
+	isSequencerActive, err := CheckSequencerActive(endpoint)
+	if err == nil {
+		t.Log("should be error")
+		t.Fail()
+	}
+	if isSequencerActive {
+		t.Log("should not active")
+		t.Fail()
+	}
+
+	// Situation 2: sequencer enable admin endpoints, but not activated
+	ms.SetIsWithAdmin(true)
+	ms.SetIsActivated(false)
+	isSequencerActive, err = CheckSequencerActive(endpoint)
+	if err != nil {
+		t.Log("should no error", err)
+		t.Fail()
+	}
+	if isSequencerActive {
+		t.Log("should not active")
+		t.Fail()
+	}
+
+	// Situation 3: sequencer enable admin endpoints, and activated
+	ms.SetIsWithAdmin(true)
+	ms.SetIsActivated(true)
+	isSequencerActive, err = CheckSequencerActive(endpoint)
+	if err != nil {
+		t.Log("should no error", err)
+		t.Fail()
+	}
+	if !isSequencerActive {
+		t.Log("should be active")
+		t.Fail()
+	}
+}
+
+func TestActivateSequencer(t *testing.T) {
+	t.Parallel()
+
+	// Prepare mock sequencer
+	ms, endpoint, err := test.NewMockSequencer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ms.Close()
+
+	// Situation 1: sequencer disable admin endpoints
+	ms.SetIsWithAdmin(false)
+	err = ActivateSequencer(endpoint, "unsafe-hash")
+	if err == nil {
+		t.Log("should be error")
+		t.Fail()
+	}
+
+	// Situation 2: sequencer enable admin endpoints, but not activated
+	ms.SetIsWithAdmin(true)
+	ms.SetIsActivated(false)
+	err = ActivateSequencer(endpoint, "unsafe-hash")
+	if err != nil {
+		t.Log("should no error", err)
+		t.Fail()
+	}
+
+	// Situation 3: sequencer enable admin endpoints, and activated
+	ms.SetIsWithAdmin(true)
+	ms.SetIsActivated(true)
+	err = ActivateSequencer(endpoint, "unsafe-hash")
+	if err == nil {
+		t.Log("should be error")
+		t.Fail()
+	}
+}
+
+func TestDeactivateSequencer(t *testing.T) {
+	t.Parallel()
+
+	// Prepare mock sequencer
+	ms, endpoint, err := test.NewMockSequencer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ms.Close()
+
+	// Situation 1: sequencer disable admin endpoints
+	ms.SetIsWithAdmin(false)
+	ms.SetUnsafeHash("unsafe-hash-1")
+	unsafeHash, err := DeactivateSequencer(endpoint)
+	if err == nil {
+		t.Log("should be error")
+		t.Fail()
+	}
+	if unsafeHash != "" {
+		t.Log("unsafeHash should be empty")
+		t.Fail()
+	}
+
+	// Situation 2: sequencer enable admin endpoints, but not activated
+	ms.SetIsWithAdmin(true)
+	ms.SetIsActivated(false)
+	ms.SetUnsafeHash("unsafe-hash-2")
+	unsafeHash, err = DeactivateSequencer(endpoint)
+	if err == nil {
+		t.Log("should be error")
+		t.Fail()
+	}
+	if unsafeHash != "" {
+		t.Log("unsafeHash should be empty")
+		t.Fail()
+	}
+
+	// Situation 3: sequencer enable admin endpoints, and activated
+	ms.SetIsWithAdmin(true)
+	ms.SetIsActivated(true)
+	ms.SetUnsafeHash("unsafe-hash-3")
+	unsafeHash, err = DeactivateSequencer(endpoint)
+	if err != nil {
+		t.Log("should no error", err)
+		t.Fail()
+	}
+	if unsafeHash != "unsafe-hash-3" {
+		t.Log("unsafeHash mismatch", unsafeHash)
+		t.Fail()
+	}
+}
+
+func TestGetOPSyncStatus(t *testing.T) {
+	t.Parallel()
+
+	// Prepare mock sequencer
+	ms, endpoint, err := test.NewMockSequencer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ms.Close()
+
+	// Situation 1: sequencer not ready
+	ms.SetIsReady(false)
+	ms.SetUnsafeHash("unsafe-hash-1")
+	unsafeHash, _, isReady, err := GetOPSyncStatus(endpoint)
+	if err != nil {
+		t.Log("should no error", err)
+		t.Fail()
+	}
+	if unsafeHash != "unsafe-hash-1" {
+		t.Log("unsafeHash should not empty")
+		t.Fail()
+	}
+	if isReady {
+		t.Log("should not ready")
+		t.Fail()
+	}
+
+	// Situation 2: sequencer ready
+	ms.SetIsReady(true)
+	ms.SetUnsafeHash("unsafe-hash-2")
+	unsafeHash, _, isReady, err = GetOPSyncStatus(endpoint)
+	if err != nil {
+		t.Log("should no error", err)
+		t.Fail()
+	}
+	if unsafeHash != "unsafe-hash-2" {
+		t.Log("unsafeHash should not empty")
+		t.Fail()
+	}
+	if !isReady {
+		t.Log("should be ready")
+		t.Fail()
+	}
+}
---
+++ test/mock_sequencer.go
@@ -0,0 +1,259 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+)
+
+/********************* Defile data types *********************/
+
+type JSONRPCRequestData struct {
+	Version string   `json:"jsonrpc"` // 2.0
+	Method  string   `json:"method"`
+	Params  []string `json:"params"`
+	ID      uint     `json:"id"` // Request ID
+}
+
+type JSONRPCResponse[T any] struct {
+	Version string                `json:"jsonrpc"` // 2.0
+	ID      uint                  `json:"id"`      // Request ID
+	Error   *JSONRPCResponseError `json:"error"`
+	Result  *T                    `json:"result"`
+}
+
+type JSONRPCResponseError struct { // Possible error
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type HeadL1Status struct {
+	// Hash       string `json:"hash"`
+	// Number     int    `json:"number"`
+	// ParentHash string `json:"parentHash"`
+	Timestamp int64 `json:"timestamp"` // For check if sequencer is ready to be activated ( 12s * 3 )
+}
+
+type UnsafeL2Status struct {
+	Hash   string `json:"hash"`
+	Number int64  `json:"number"`
+	// Timestamp int64 `json:"timestamp"` // Not for isReady status reference
+}
+
+type OPSyncStatus struct { // Ignore irrelevant fields
+	HeadL1   HeadL1Status   `json:"head_l1"`
+	UnsafeL2 UnsafeL2Status `json:"unsafe_l2"`
+}
+
+/********************* Initialize mock sequencer *********************/
+
+type MockSequencer struct {
+	listener net.Listener
+	server   http.Server
+
+	isWithAdmin bool // Can be activated
+	isActivated bool // Is now activated
+	isReady     bool // Is sync with mainnet
+
+	unsafeHash string // Unsafe L2 block hash
+}
+
+func NewMockSequencer() (*MockSequencer, string, error) {
+	// Create a mock sequencer object
+	ms := &MockSequencer{
+		isActivated: false,
+	}
+
+	var err error
+
+	// Listen with a random port
+	ms.listener, err = net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return nil, "", err
+	}
+
+	// Prepare handlers
+	mockSequencerMux := http.NewServeMux()
+	mockSequencerMux.HandleFunc("/", ms.handleJSONRPC)
+
+	// Prepare server
+	ms.server = http.Server{
+		Handler: mockSequencerMux,
+	}
+
+	// Server start
+	go func() {
+		err := ms.server.Serve(ms.listener)
+		if err != nil {
+			_ = ms.listener.Close()
+		}
+	}()
+
+	// Finish create
+	return ms, fmt.Sprintf("http://%s/", ms.listener.Addr().String()), nil
+}
+
+/********************* Handle JSONRPC requests *********************/
+
+func noSuchMethod(method string) string {
+	return fmt.Sprintf("the method %s does not exist/is not available", method)
+}
+
+func (ms *MockSequencer) handleJSONRPC(w http.ResponseWriter, req *http.Request) {
+	// Parse request
+	var reqBody JSONRPCRequestData
+
+	err := json.NewDecoder(req.Body).Decode(&reqBody)
+	if err != nil {
+		// Fail to decode body
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Prepare response
+	var resBodyBytes []byte
+
+	switch reqBody.Method {
+	case "admin_sequencerActive":
+		resBody := JSONRPCResponse[bool]{
+			Version: reqBody.Version,
+			ID:      reqBody.ID,
+		}
+
+		if ms.isWithAdmin {
+			resBody.Result = &ms.isActivated
+		} else {
+			resBody.Error = &JSONRPCResponseError{
+				-32601,
+				noSuchMethod("admin_sequencerActive"),
+			}
+		}
+
+		resBodyBytes, _ = json.Marshal(&resBody)
+
+	case "admin_startSequencer":
+		resBody := JSONRPCResponse[any]{
+			Version: reqBody.Version,
+			ID:      reqBody.ID,
+		}
+
+		if ms.isWithAdmin {
+			if ms.isActivated {
+				resBody.Error = &JSONRPCResponseError{
+					-32000,
+					"sequencer already running",
+				}
+			} else {
+				ms.unsafeHash = reqBody.Params[0]
+				ms.isActivated = true
+			}
+		} else {
+			resBody.Error = &JSONRPCResponseError{
+				-32601,
+				noSuchMethod("admin_startSequencer"),
+			}
+		}
+
+		resBodyBytes, _ = json.Marshal(&resBody)
+
+	case "admin_stopSequencer":
+		resBody := JSONRPCResponse[string]{
+			Version: reqBody.Version,
+			ID:      reqBody.ID,
+		}
+
+		if ms.isWithAdmin {
+			if !ms.isActivated {
+				resBody.Error = &JSONRPCResponseError{
+					-32000,
+					"sequencer not running",
+				}
+			} else {
+				resBody.Result = &ms.unsafeHash
+				ms.isActivated = false
+			}
+		} else {
+			resBody.Error = &JSONRPCResponseError{
+				-32601,
+				noSuchMethod("admin_stopSequencer"),
+			}
+		}
+
+		resBodyBytes, _ = json.Marshal(&resBody)
+
+	case "optimism_syncStatus":
+		resBody := JSONRPCResponse[OPSyncStatus]{
+			Version: reqBody.Version,
+			ID:      reqBody.ID,
+		}
+
+		resBody.Result = &OPSyncStatus{
+			HeadL1: HeadL1Status{
+				Timestamp: 0,
+			},
+			UnsafeL2: UnsafeL2Status{
+				Hash: ms.unsafeHash,
+			},
+		}
+
+		if ms.isReady {
+			resBody.Result.HeadL1.Timestamp = time.Now().Unix()
+		}
+
+		resBodyBytes, _ = json.Marshal(&resBody)
+
+	default:
+		var resBody JSONRPCResponse[any]
+
+		resBody.Error = &JSONRPCResponseError{
+			-32601,
+			noSuchMethod(reqBody.Method),
+		}
+
+		resBodyBytes, _ = json.Marshal(&resBody)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(resBodyBytes)
+}
+
+func (ms *MockSequencer) Close() {
+	_ = ms.server.Close()
+	_ = ms.listener.Close()
+}
+
+/********************* Manage mock sequencer status *********************/
+
+func (ms *MockSequencer) SetIsWithAdmin(isWithAdmin bool) {
+	ms.isWithAdmin = isWithAdmin
+}
+
+func (ms *MockSequencer) GetIsWithAdmin() bool {
+	return ms.isWithAdmin
+}
+
+func (ms *MockSequencer) SetIsActivated(isActivated bool) {
+	ms.isActivated = isActivated
+}
+
+func (ms *MockSequencer) GetIsActivated() bool {
+	return ms.isActivated
+}
+
+func (ms *MockSequencer) SetIsReady(isReady bool) {
+	ms.isReady = isReady
+}
+
+func (ms *MockSequencer) GetIsReady() bool {
+	return ms.isReady
+}
+
+func (ms *MockSequencer) SetUnsafeHash(hash string) {
+	ms.unsafeHash = hash
+}
+
+func (ms *MockSequencer) GetUnsafeHash() string {
+	return ms.unsafeHash
+}
